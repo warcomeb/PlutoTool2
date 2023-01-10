bf9fda911d8780325932da329096c8df79b39540
Marco Giammarini <m.giammarini@warcomeb.it>
2023-01-10 13:32:04 +0100

    .

---
+++ PlutoTool/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PlutoTool.Models;
+
+namespace PlutoTool.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+        public string NameParent { get; set; }
+    }
+}
