a9804bc44a698895af8f6800cd46fc348968ca1a
Marco Giammarini <m.giammarini@warcomeb.it>
2023-01-10 13:20:25 +0100

    .

--- PlutoTool/Database/PlutoDbContext.cs
+++ PlutoTool/Database/PlutoDbContext.cs
@@ -14,10 +14,14 @@ namespace PlutoTool.Database
         public DbSet<User> User { get; set; } = null!;
         public DbSet<Account> Account { get; set; } = null!;
         public DbSet<Category> Category { get; set; } = null!;
-        public DbSet<SubCategory> SubCategory { get; set; } = null!;
         public DbSet<Payee> Payee { get; set; } = null!;
-        public DbSet<PayeeType> PayeeType { get; set; } = null!;
         public DbSet<WorkOrder> WorkOrder { get; set; } = null!;
         public DbSet<Vehicle> Vehicle { get; set; } = null!;
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Account>()
+            //    .HasF(p => p.OwnerUser).WithRequired(p => p.Instructor);
+        }
     }
 }
